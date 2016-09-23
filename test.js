f47dc9303b9a9acf7980617f7920ebc81eb3c547
Dominic Barrett <dominic.barrett@nyu.edu>
2016-09-22 22:11:54 -0400

    my first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+}
+
+function mousePressed(){
+ background(255,0,0);
+  debugger;
+  //console.log('its red');
+}
